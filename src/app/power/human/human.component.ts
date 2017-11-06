fc823c9e1d27b51810a5a193368d01fde46e4d8f
Jiahao Yu <ilywanzi@gmail.com>
2017-11-06 12:24:19 -0800

    first

---
+++ src/app/power/human/human.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-human',
+  templateUrl: './human.component.html',
+  styleUrls: ['./human.component.css']
+})
+export class HumanComponent implements OnInit, OnChanges {
+  @Input() power;
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+  ngOnChanges() {
+    this.power = this.power * 1;
+  }
+  
+  
+  
+}
